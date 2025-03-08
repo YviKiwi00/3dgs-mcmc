14bcdfa972d38370ede1a291b5e2ef34e4305fbe
Yvette Gyarmati <yvette@yvette-gyarmati.de>
2025-03-08 13:28:48 +0100

    modified to work with sagd depth and alpha

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -82,7 +82,7 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
         colors_precomp = override_color
 
     # Rasterize visible Gaussians to image, obtain their radii (on screen). 
-    rendered_image, radii, is_used = rasterizer(
+    rendered_image, radii, rendered_depth, rendered_alpha, is_used = rasterizer(
         means3D = means3D,
         means2D = means2D,
         shs = shs,
